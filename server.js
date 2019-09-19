af36975b45dc7e94d38bc597556ce6bec5611a8d
Dwika Setya <dwikasm@gmail.com>
2019-09-19 12:52:01 +0700

    add route fallback

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
 
 const app = express();
 
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+  // OPTIONAL: Includes more verbose logging
+  verbose: true,
+}));
+
+
 // app.use(serveStatic(`${__dirname}/dist`));
-app.use(serveStatic(`${path.dirname(__filename)}/dist`));
+// app.use(serveStatic(`${path.dirname(__filename)}/dist`));
+app.use(serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 5000;
 app.listen(port);
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,10 @@ import PokeDetail from '../pages/PokeDetail.vue';
 export default [
   {
     path: '/',
+    redirect: { name: 'pokeDetail' },
+  },
+  {
+    path: '/poke-detail',
     name: 'pokeDetail',
     component: PokeDetail,
   },

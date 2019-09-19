872e1d548845855cbe46b5a1c97cce7b7cc3c697
Dwika Setya <dwikasm@gmail.com>
2019-09-19 10:15:27 +0700

    update to simpler route

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,6 @@ import PokeDetail from '../pages/PokeDetail.vue';
 export default [
   {
     path: '/',
-    redirect: { name: 'pokeDetail' },
-  },
-  {
-    path: '/poke-detail',
     name: 'pokeDetail',
     component: PokeDetail,
   },
